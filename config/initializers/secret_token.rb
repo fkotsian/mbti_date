d51893e320422d41bb237cdae5755ca11da320a1
Frank Kotsianas <frankkotsianas@gmail.com>
2014-05-04 19:39:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MbtiDate::Application.config.secret_key_base = '6e273ae9260d5fb8012f3d9763e1bf577639c45b7622a6f140e408ca87b73eb18a231323569337930d3c75081ebcb7c3cab0596759883f790e11631ee814b972'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MbtiDate::Application.config.session_store :cookie_store, key: '_mbti_date_session'
